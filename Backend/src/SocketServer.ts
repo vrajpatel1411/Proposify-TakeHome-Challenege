97586ec45583ae9c271181ef90220566c9b0949f
Vraj Patel <vraj.patel.12390@gmail.com>
2025-01-28 01:21:08 -0500

    Add real-time content sync for collaborative editing
    - Implemented WebSocket server using socket.io to manage real-time communication.
    - Added logic to store the latest document content in memory.
    - Ensured new users receive the current document content upon connection.
    - Broadcast content updates to all connected users in real time.

--- Backend/src/SocketServer.ts
+++ Backend/src/SocketServer.ts
@@ -4,10 +4,23 @@ import { Server as HttpsServer } from "https";
 
 const startSocketServer = (httpServer: HttpServer | HttpsServer) => {
   const io: SocketServer = new SocketServer(httpServer, { cors: {} });
-
+  let currentContent: String =
+    "Write your story, ideas, or blog post here. Let your creativity flow!";
   const onConnection = async (socket: Socket) => {
-    // Socket connection started
     console.log("Connection started with socket id: " + socket.id);
+
+    socket.emit("contentUpdate", currentContent);
+    // Socket connection started
+    socket.on("contentUpdate", (content: string) => {
+      console.log("Received updated content:", content);
+      currentContent = content;
+      // Broadcast the updated content to all connected clients
+      io.emit("contentUpdate", content);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected: " + socket.id);
+    });
   };
 
   io.on("connection", onConnection);
