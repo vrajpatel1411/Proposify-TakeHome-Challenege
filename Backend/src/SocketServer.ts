11808bd535a4583b87c2cc21014746ee9d64803c
Vraj Patel <vraj.patel.12390@gmail.com>
2025-01-27 20:04:57 -0500

    Project is intialized and restructured

---
+++ Backend/src/SocketServer.ts
@@ -0,0 +1,18 @@
+import { Server as SocketServer, Socket } from "socket.io";
+import { Server as HttpServer } from "http";
+import { Server as HttpsServer } from "https";
+
+const startSocketServer = (httpServer: HttpServer | HttpsServer) => {
+  const io: SocketServer = new SocketServer(httpServer, { cors: {} });
+
+  const onConnection = async (socket: Socket) => {
+    // Socket connection started
+    console.log("Connection started with socket id: " + socket.id);
+  };
+
+  io.on("connection", onConnection);
+
+  console.log("Websocket server is running..");
+};
+
+export default startSocketServer;
